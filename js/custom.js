009509d7d21dc73ce7e55f1c5dca1e7496c831d9
Rehab5 <69490304+Rehab5@users.noreply.github.com>
2021-01-08 17:07:28 +0200

    test

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,44 @@
+
+
+alert("Welcome to my webpage Front-End \n Rehab Kosbar");
+
+//add User function
+var userdetails ={
+    name:'',
+    email:'',
+    phone:'',
+    message:''
+};
+var Users=[];
+function addUser(event){
+    //remove the behaviour of page(refresh)
+    event.preventDefault();
+    
+    //store input value
+    var name= document.getElementById('name');
+    var email= document.getElementById('email');
+    var phone= document.getElementById('phone');
+    var message= document.getElementById('message');
+    
+    // bind the input value in userDetails object
+    userdetails.name =name.value;
+    userdetails.email= email.value;
+    userdetails.phone = phone.value;
+    userdetails.message =message.value;
+    
+    //add user data on array
+    Users.push(userdetails);
+	
+    userdetails={};
+   
+    name.value='';
+    email.value ='';
+    phone.value ='';
+    message.value='';
+    
+};
+
+
 //Button up
   $(window).scroll(function () {
     if ($(window).scrollTop() >= 1000) {
